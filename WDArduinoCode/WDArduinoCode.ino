489a637b96811a4e871de147a1dcbd6586f6891e
NPA <nicbe@hotmail.fi>
2018-07-17 21:38:33 +0300

    Code to check display works.

--- WDArduinoCode/WDArduinoCode.ino
+++ WDArduinoCode/WDArduinoCode.ino
@@ -1,9 +1,25 @@
+#include <GxEPD.h>
+#include <GxGDE0213B1/GxGDE0213B1.cpp>
+#include <GxIO/GxIO_SPI/GxIO_SPI.cpp>
+#include <GxIO/GxIO.cpp>
+
+// Initialize SPI
+GxIO_Class io(SPI, SS, D3, D4);
+// Adafruit GFX compatible class
+GxEPD_Class display(io);
+
 void setup() {
-  // put your setup code here, to run once:
+  // Initialize display
+  display.init();
+  // Set black text
+  display.setTextColor(GxEPD_BLACK);
+  // Text location: x = 30, y = 60
+  display.setCursor(30,60);
+  display.print("Sun shines");
+  // Update commands to screen
+  display.update();
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
